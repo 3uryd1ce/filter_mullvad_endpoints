747611aa88d82843434470d926831a2312e3c29d
Ashlen <dev@anthes.is>
2023-09-23 12:38:16 -0600

    test(compile_regex): split tests, add docstrings

--- tests/test_compile_regex.py
+++ tests/test_compile_regex.py
@@ -4,12 +4,20 @@ import re
 from filter_mullvad_endpoints import compile_regex
 
 
-def test_compile_regex():
-    # Test valid regular expression pattern
+def test_valid_regex():
+    """
+    Make sure compile_regex returns a re.Pattern object when a valid
+    regex is provided.
+    """
     pattern = r"^it-(mil|rom)$"
     assert isinstance(compile_regex(pattern), re.Pattern)
 
-    # Test invalid regular expression pattern
+
+def test_invalid_regex():
+    """
+    Make sure compile_regex throws an exception when an invalid
+    regex is provided.
+    """
     pattern = r"["
     try:
         compile_regex(pattern)
@@ -17,11 +25,3 @@ def test_compile_regex():
         assert e.code == 1
     else:
         assert False, "Expected SystemExit exception"
-
-    # Test regular expression pattern with special characters
-    pattern = r"[a-zA-Z0-9_]+"
-    assert isinstance(compile_regex(pattern), re.Pattern)
-
-    # Test regular expression pattern with quantifiers
-    pattern = r"\d{3}-\d{3}-\d{4}"
-    assert isinstance(compile_regex(pattern), re.Pattern)
